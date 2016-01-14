418af866cff5bf5b9fb0354c4bff9e09125e7958
Nic Netzel and torrence stratton <tlstratton@gmail.com>
2016-01-13 16:00:40 -0800

    add 99 problems

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+  var counter = 100;
+  var counterless = 0;
+  for (var index = 99; index >= 0; index += -1) {
+    counter = (counter - 1);
+    counterless = (counter - 1);
+    $(".test").append('<p>' + counter + ' bottles of beer on the wall, ' + counter + ' bottles of beer.' + "<br>" + 'Take one down and pass it around,' + counterless + ' bottles of beer on the wall.' + '</p>');
 
-
+  }
 
 });
+
+
+// <span class="current-number"></span> bottles of beer on the wall, <span class="current-number"></span>  bottles of beer.
+// Take one down and pass it around, <span class="less-number"></span>  bottles of beer on the wall.
+// </p>
